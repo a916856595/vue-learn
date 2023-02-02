1ea10146e0682ac9db5bb10b9cea65f2c75e7579
george-hong <hcj147@vip.qq.com>
2023-02-02 16:48:10 +0800

    feat: 响应系统基础示例

--- practices/002.响应系统/002.解决分支切换的问题/proxy.js
+++ practices/002.响应系统/002.解决分支切换的问题/proxy.js
@@ -1,4 +1,6 @@
-// page 41
+// page 50
+
+// 要解决多余副作用函数的问题，需要在每次副作用函数执行时将其从集合中删除
 
 // 临时存储副作用函数
 let activeEffect;
@@ -7,10 +9,30 @@ const bucket = new WeakMap();
 
 // 触发绑定映射关系函数
 function effect(fn) {
-  // 存储副作用函数
-  activeEffect = fn;
-  // 调用副作用函数以触发代理的getter
-  fn();
+  const doEffect = function() {
+    // 执行副作用前清空对当前副作用的引用
+    cleanup(doEffect);
+    // 存储副作用函数及dependence的引用
+    activeEffect = doEffect;
+    // 调用副作用函数以触发代理的getter
+    fn();
+  }
+
+  // doEffect.dependence用于存储包含此副作用函数的集合,用于清除集合中的effect
+  doEffect.dependence = [];
+  // 开始执行
+  doEffect();
+}
+
+// 清空缓存的副作用函数绑定的依赖集合
+function cleanup(doEffect) {
+  // 从集合中删除副作用函数
+  doEffect.dependence.forEach(effectSet => {
+    effectSet.delete(doEffect);
+  });
+
+  // 重置使用此副作用函数集合的集
+  doEffect.dependence.length = 0;
 }
 
 // 绑定映射关系的函数
@@ -39,6 +61,8 @@ function track(target, key) {
 
   // 存储副作用函数到对应的集合中
   effectSet.add(activeEffect);
+  // 将集合存储到使用副作用函数的集合中，用于清空集合内的副作用函数
+  activeEffect.dependence.push(effectSet);
   // 可能在一次副作用函数中读取多次属性值，因此不能清空记录的副作用函数
 }
 
@@ -53,8 +77,11 @@ function trigger(target, key) {
   const effectSet = targetMapping.get(key);
   if (!effectSet) return;
 
+  // doEffect()中会调用cleanup清除副作用，随后fn又重新添加副作用，最终导致effectSet无限循环
+  // 因此需要使用新的Set集合来执行循环，避免无限循环。详见 page 55
+  const effectSetCloned = new Set(effectSet);
   // 依次触发副作用函数
-  effectSet.forEach(effect => effect());
+  effectSetCloned.forEach(effect => effect());
 }
 
 
--- practices/002.响应系统/003.支持effect嵌套/proxy.js
+++ practices/002.响应系统/003.支持effect嵌套/proxy.js
@@ -1,16 +1,46 @@
-// page 41
+// page 56
+
+// 要解决多余副作用函数的问题，需要在每次副作用函数执行时将其从集合中删除
 
 // 临时存储副作用函数
 let activeEffect;
+// 存储副作用函数的栈
+const effectStack = [];
 // 对象与副作用函数的对应关系
 const bucket = new WeakMap();
 
 // 触发绑定映射关系函数
 function effect(fn) {
-  // 存储副作用函数
-  activeEffect = fn;
-  // 调用副作用函数以触发代理的getter
-  fn();
+  const doEffect = function() {
+    // 执行副作用前清空对当前副作用的引用
+    cleanup(doEffect);
+    // 存储副作用函数及dependence的引用
+    activeEffect = doEffect;
+    // 将副作用函数存入栈中，以支持嵌套effect
+    effectStack.push(activeEffect);
+    // 调用副作用函数以触发代理的getter（收集副作用）
+    fn();
+    // 在副作用收集完成后，移除栈中的最后一项
+    effectStack.pop();
+    // 将副作用记录值更新为上层的副作用，这样就能支持嵌套的effect
+    activeEffect = effectStack[effectStack.length - 1];
+  }
+
+  // doEffect.dependence用于存储包含此副作用函数的集合,用于清除集合中的effect
+  doEffect.dependence = [];
+  // 开始执行
+  doEffect();
+}
+
+// 清空缓存的副作用函数绑定的依赖集合
+function cleanup(doEffect) {
+  // 从集合中删除副作用函数
+  doEffect.dependence.forEach(effectSet => {
+    effectSet.delete(doEffect);
+  });
+
+  // 重置使用此副作用函数集合的集
+  doEffect.dependence.length = 0;
 }
 
 // 绑定映射关系的函数
@@ -39,6 +69,8 @@ function track(target, key) {
 
   // 存储副作用函数到对应的集合中
   effectSet.add(activeEffect);
+  // 将集合存储到使用副作用函数的集合中，用于清空集合内的副作用函数
+  activeEffect.dependence.push(effectSet);
   // 可能在一次副作用函数中读取多次属性值，因此不能清空记录的副作用函数
 }
 
@@ -53,8 +85,11 @@ function trigger(target, key) {
   const effectSet = targetMapping.get(key);
   if (!effectSet) return;
 
+  // doEffect()中会调用cleanup清除副作用，随后fn又重新添加副作用，最终导致effectSet无限循环
+  // 因此需要使用新的Set集合来执行循环，避免无限循环。详见 page 55
+  const effectSetCloned = new Set(effectSet);
   // 依次触发副作用函数
-  effectSet.forEach(effect => effect());
+  effectSetCloned.forEach(effect => effect());
 }
 
 
